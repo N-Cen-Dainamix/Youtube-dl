423b7327f5584b9745bc18f9a5263d228365b1d2
Admin N-Cen-Dainamix <ncendainamix@gmail.com>
2019-10-17 00:39:41 +0700

    Youtube Automatic Converter Downloader [installer]

--- install.sh
+++ install.sh
@@ -25,37 +25,26 @@ echo "+++ Creates a Download Directory on: storage/dcim/DM-Termux";
 sleep 2;
 mkdir /data/data/com.termux/files/home/storage/dcim/DM-Termux
 echo "+++ Install Python +++"
-echo "\\n"
 sleep 2;
 pkg install python -y
-echo "\\n"
 echo "+++ Install Perl (For Youtube Automatic Converter Downloader ) +++"
-echo "\\n"
 sleep 2;
 pkg install perl -y
-echo "\\n"
 echo "+++ Install cURL +++"
-echo "\\n"
 sleep 2;
 pkg install curl -y
-echo "\\n"
 echo "+++ Install ffmpeg (For Audio Conversion) +++"
-echo "\\n"
 sleep 2;
 pkg install ffmpeg -y
-echo "\\n"
-echo "\\n"
 echo "+++ Downloading Youtube-dl +++"
 sleep 3;
 curl -s -L https://yt-dl.org/downloads/latest/youtube-dl -o /data/data/com.termux/files/usr/bin/youtube-dl
 echo "+++ Youtube-dl Downloaded Successfully +++"
-echo "\\n"
 sleep 2;
 echo "+++ Set Permission to Execute the Script +++"
 sleep 2;
 chmod a+rx /data/data/com.termux/files/usr/bin/youtube-dl
 echo "+++ Permission Granted +++"
-echo "\\n"
 sleep 2;
 echo "+++ Setup Startup +++"
 sleep 2;
@@ -64,7 +53,5 @@ cp /data/data/com.termux/files/home/Youtube-dl/yt-downloader.pl /data/data/com.t
 echo clear >> ../usr/etc/bash.bashrc
 echo cd storage/dcim/DM-Termux >> ../usr/etc/bash.bashrc
 echo perl yt-downloader.pl >> ../usr/etc/bash.bashrc
-echo "\\n"
 echo "+++ Installation completed successfully +++"
-echo "+++ Reboot Termux +++";
-sleep 5;
+echo "+++ Reboot Termux +++"
